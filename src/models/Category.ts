099192c0e87ddc65786621250fcbcdc6034d407d
Welben Costa <welben@MacBook-Air-de-Admin.local>
2021-03-25 11:31:34 -0300

    Ajustes nas models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -4,8 +4,10 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
 
+import Transaction from './Transaction';
 @Entity('categories')
 class Category {
   @PrimaryGeneratedColumn('uuid')
@@ -13,6 +15,9 @@ class Category {
 
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @CreateDateColumn()
   created_at: Date;
